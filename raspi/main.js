ed27385c1cb754a30c1778d3c6917889f4aa7e99
Axel Mousset <pymzorr@gmail.com>
2014-12-05 14:31:21 +0100

    Add support of serial port option

--- raspi/main.js
+++ raspi/main.js
@@ -4,34 +4,40 @@ var MotorController = require('./MotorController');
 var serialPort = require('serialport');
 var readline = require('readline');
 var log = require('./libs').logger(module);
+var config = require('./libs').config;
 
 
-serialPort.list(function (err, ports) {
-    if (err) {
-        log.error(err);
-        return;
-    }
-
-    if (ports.length === 0) {
-        log.error('No open ports');
-    }
-    else if (ports.length === 1) {
-        var motorController = new MotorController(ports[0].comName);
-    }
-    else {
-        for (var i = 0; i < ports.length; i++) {
-            log.info(i + ': ' + ports[i].comName);
+if (config.get('port')) {
+    var motorController = new MotorController(config.get('port'));
+}
+else {
+    serialPort.list(function (err, ports) {
+        if (err) {
+            log.error(err);
+            return;
         }
 
-        var rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+        if (ports.length === 0) {
+            log.error('No open ports');
+        }
+        else if (ports.length === 1) {
+            var motorController = new MotorController(ports[0].comName);
+        }
+        else {
+            for (var i = 0; i < ports.length; i++) {
+                log.info(i + ': ' + ports[i].comName);
+            }
 
-        log.info('Choose a com port: ');
+            var rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
 
-        rl.question('', function(answer) {
-            var motorController = new MotorController(ports[parseInt(answer)].comName);
-        });
-    }
-});
+            log.info('Choose a com port: ');
+
+            rl.question('', function(answer) {
+                var motorController = new MotorController(ports[parseInt(answer)].comName);
+            });
+        }
+    });    
+}
