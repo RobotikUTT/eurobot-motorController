78657c0d4085d75b24714a5d7f1a766f6cd31e1b
Axel Mousset <pymzorr@gmail.com>
2014-12-05 14:27:15 +0100

    Add menu to choose which serial port to use

--- raspi/main.js
+++ raspi/main.js
@@ -1,6 +1,37 @@
 'use strict';
 
 var MotorController = require('./MotorController');
+var serialPort = require('serialport');
+var readline = require('readline');
+var log = require('./libs').logger(module);
 
 
-var motorController = new MotorController('/dev/ttyACM3');
+serialPort.list(function (err, ports) {
+    if (err) {
+        log.error(err);
+        return;
+    }
+
+    if (ports.length === 0) {
+        log.error('No open ports');
+    }
+    else if (ports.length === 1) {
+        var motorController = new MotorController(ports[0].comName);
+    }
+    else {
+        for (var i = 0; i < ports.length; i++) {
+            log.info(i + ': ' + ports[i].comName);
+        }
+
+        var rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        log.info('Choose a com port: ');
+
+        rl.question('', function(answer) {
+            var motorController = new MotorController(ports[parseInt(answer)].comName);
+        });
+    }
+});
