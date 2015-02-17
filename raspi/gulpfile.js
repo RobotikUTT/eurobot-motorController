76021fdfd60d51c9743d306ef3c24751dfb45f93
Axel Mousset <pymzorr@gmail.com>
2015-02-17 21:07:18 +0100

    Add error support for gulp when building files

--- raspi/gulpfile.js
+++ raspi/gulpfile.js
@@ -2,9 +2,16 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 gulp.task('build', function () {
     gulp.src('src/**/*.js')
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(sourcemaps.write('.'))
