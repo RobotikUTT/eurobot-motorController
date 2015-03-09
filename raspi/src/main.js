bd8e45d31532111a5c49d199d7d1438d293e9af5
Axel Mousset <pymzorr@gmail.com>
2015-03-09 23:04:42 +0100

    Move raspi project into another git repo : eurobot-core

--- raspi/src/main.js
+++
@@ -1,6 +0,0 @@
-import MotorController from './modules/MotorController';
-import * as controlPannel from './controlPannel/server';
-
-
-controlPannel.start();
-var motorController = new MotorController();
