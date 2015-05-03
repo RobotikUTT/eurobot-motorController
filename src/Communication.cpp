234c2d8939941988f32e8c2498b315a64955a11a
Aurélien Labate <aurelien-git@labate.me>
2015-05-03 13:26:56 +0200

    Update the ping packet to make it match to the server

--- src/Communication.cpp
+++ src/Communication.cpp
@@ -2,10 +2,6 @@
 
 
 byte Communication::lastCmd = 0xff;
-byte Communication::lastPingU8 = 0;
-char Communication::lastPingS8 = 0;
-unsigned short Communication::lastPingU16 = 0;
-short Communication::lastPingS16 = 0;
 Odometry* Communication::odometry = Odometry::getInst();
 Enslavement* Communication::enslavement;
 
@@ -42,20 +38,11 @@ void Communication::execute(byte command, byte length, byte* params)
             if(length >= 6)
             {
                 byte pos = 0;
-                Communication::lastPingU8 = extractUInt8(&pos, params);
-                Communication::lastPingS8 = extractInt8(&pos, params);
-                Communication::lastPingU16 = extractUInt16(&pos, params);
-                Communication::lastPingS16 = extractInt16(&pos, params);
-
-                // Debug
-                // Serial.print("Ping recu :");
-                // Serial.print(Communication::lastPingU8, DEC);
-                // Serial.print(" : ");
-                // Serial.print(Communication::lastPingS8, DEC);
-                // Serial.print(" : ");
-                // Serial.print(Communication::lastPingU16, DEC);
-                // Serial.print(" : ");
-                // Serial.println(Communication::lastPingS16, DEC);
+                byte val = extractUInt8(&pos, params);
+                if(val != Communication::address)
+                {
+                    Serial.println("Warning : Bad ping address received !");
+                }
             }
             break;
         }
@@ -164,11 +151,7 @@ void Communication::send()
     {
         case Communication::cmd_ping:
         {
-            Serial.println("Send ping");
-            Communication::addUInt8(Communication::lastPingU8 + 1);
-            Communication::addInt8(Communication::lastPingS8 + 1);
-            Communication::addUInt16(Communication::lastPingU16 + 1);
-            Communication::addInt16(Communication::lastPingS16 + 1);
+            Communication::addUInt8(Communication::address);
             break;
         }
         case Communication::cmd_get_odometry:
